b48256eed813b91fbf2fb4e22ae0bdf873fb31c0
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-11-09 20:58:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    background('#ffbb78');
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Intialized!');
+}
+
+function gotPoses(results){
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
